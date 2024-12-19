783da95c6d6e7c4132d771a332367038ff177099
jinjaelee <jjlee009@newsystech.co.kr>
2024-12-19 15:20:50 +0900

    emotion util

--- src/App.js
+++ src/App.js
@@ -1,8 +1,12 @@
 import './App.css';
+import emotion1 from "./img/emotion1.png";
 
 function App() {
   return (
-    <div className="App"></div>
+    <div className="App">
+      <h1>감정 일기장</h1>
+      <img alt="감정1" src={emotion1} />
+    </div>
   );
 }
 
