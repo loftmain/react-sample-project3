35f03f4c2e281c6983121d9e1baa2bc1e03ab6ae
jinjaelee <jjlee009@newsystech.co.kr>
2024-12-20 13:59:13 +0900

    기본 css

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,19 +1,6 @@
-import Editor from "../component/Editor";
-
 const Home = () => {
     return (
-        <div>
-            <Editor 
-                initData={{
-                    date: new Date().getTime(),
-                    emotionId: 1,
-                    content: "이전에 작성했던 일기",
-                }} 
-                onSubmit={() => {
-                    alert("작성 완료 버튼을 클릭");
-                }} 
-            />
-        </div>
+        <div>Home 페이지입니다.</div>
     );
 }
 export default Home;
