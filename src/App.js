f16504312d264ba6ac967d5f7612663850e6e447
jinjaelee <jjlee009@newsystech.co.kr>
2024-12-20 10:12:49 +0900

    공통 컴포넌트 Header

--- src/App.js
+++ src/App.js
@@ -1,11 +1,25 @@
+import { Link, Route, Routes } from 'react-router-dom';
 import './App.css';
-import emotion1 from "./img/emotion1.png";
+import Home from './pages/Home';
+import New from './pages/New';
+import Diary from './pages/Diary';
+import Edit from './pages/Edit';
 
 function App() {
   return (
     <div className="App">
-      <h1>감정 일기장</h1>
-      <img alt="감정1" src={emotion1} />
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/new" element={<New />} />
+        <Route path="/diary/:id" element={<Diary />} />
+        <Route path="/edit" element={<Edit />} />
+      </Routes>      
+      <div>
+        <Link to={"/"}>Home</Link>
+        <Link to={"/new"}>New</Link>
+        <Link to={"/diary"}>Diary</Link>
+        <Link to={"/edit"}>Edit</Link>
+      </div>
     </div>
   );
 }
---
+++ src/pages/Home.js
@@ -0,0 +1,31 @@
+import Button from "../component/Button";
+import Header from "../component/Header";
+
+const Home = () => {
+    return (
+        <div>
+            <Header
+                title={"Home"}
+                leftChild={
+                    <Button
+                        type="positive"
+                        text={"긍정 버튼"}
+                        onClick={() => {
+                            alert("positive button");
+                        }}
+                    />
+                }
+                rightChild={
+                    <Button
+                        type="negative"
+                        text={"부정 버튼"}
+                        onClick={() => {
+                            alert("negative button");
+                        }}
+                    />
+                }
+            />
+        </div>
+    );
+}
+export default Home;
